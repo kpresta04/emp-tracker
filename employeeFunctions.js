22976a6883e9b2dfaaf79399e032c88820d6660b
Kellen Presta <kpresta04@gmail.com>
2020-03-05 17:43:45 -0600

    async added to employee functions

--- employeeFunctions.js
+++ employeeFunctions.js
@@ -20,42 +20,41 @@ const alphaValidator = input => {
 };
 
 const runEmployeePrompts = async () => {
-  await inquirer
-    .prompt([
-      {
-        name: "first_name",
-        message: "Enter employee's first name: ",
-        type: "input",
-        validate: alphaValidator
-      },
-      {
-        name: "last_name",
-        message: "Enter employee's last name: ",
-        type: "input",
-        validate: alphaValidator
-      },
-      {
-        name: "role_id",
-        message: "Enter employee's role ID: ",
-        type: "list",
-        choices: ["1: Manager", "2: Engineer", "3: Intern"]
-      },
-      {
-        name: "manager_id",
-        message: "Enter the ID of the employee's manager: ",
-        type: "input",
-        validate: intValidator,
-        default: "Enter a number or press Enter to skip"
-      }
-    ])
-    .then(answers => {
-      answers.role_id = Number(answers.role_id.slice(0, 1)); // We only want the integer
-      if (answers.manager_id === "Enter a number or press Enter to skip") {
-        // Remove if default
-        answers.manager_id = null;
-      } else answers.manager_id = Number(answers.manager_id);
-      return answers;
-    });
+  let answers = await inquirer.prompt([
+    {
+      name: "first_name",
+      message: "Enter employee's first name: ",
+      type: "input",
+      validate: alphaValidator
+    },
+    {
+      name: "last_name",
+      message: "Enter employee's last name: ",
+      type: "input",
+      validate: alphaValidator
+    },
+    {
+      name: "role_id",
+      message: "Enter employee's role ID: ",
+      type: "list",
+      choices: ["1: Manager", "2: Engineer", "3: Intern"]
+    },
+    {
+      name: "manager_id",
+      message: "Enter the ID of the employee's manager: ",
+      type: "input",
+      validate: intValidator,
+      default: "Enter a number or press Enter to skip"
+    }
+  ]);
+  // Transform answers
+
+  answers.role_id = Number(answers.role_id.slice(0, 1)); // We only want the integer
+  if (answers.manager_id === "Enter a number or press Enter to skip") {
+    // Remove if default
+    answers.manager_id = null;
+  } else answers.manager_id = Number(answers.manager_id);
+  return answers;
 };
 
 exports.addNewEmployee = async connection => {
